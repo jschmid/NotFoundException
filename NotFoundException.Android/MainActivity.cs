ae8542e04f3ae03a9305c1c40b34046c85f992b8
Jonas Schmid <jonas.schmid@gmail.com>
2019-07-10 11:05:55 +0200

    Showing a loading prompt

--- NotFoundException.Android/MainActivity.cs
+++ NotFoundException.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Acr.UserDialogs;
 
 namespace NotFoundException.Droid
 {
@@ -22,6 +23,9 @@ namespace NotFoundException.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            UserDialogs.Init(this);
+            UserDialogs.Instance.ShowLoading();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
